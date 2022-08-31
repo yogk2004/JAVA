d49a31791ed69352967ddb71f1e36a50b0631490
yogk2004 <yog.k2004@gmail.com>
2022-08-31 14:08:15 +0530

    Problem On Printing

---
+++ Problem1/src/PrintingProblem1.java
@@ -0,0 +1,11 @@
+public class PrintingProblem1 {
+    public static void main(String[] args) {
+        System.out.println("This is My First Java Application");
+
+        System.out.println("==================================");
+
+        System.out.println("My Name is: Yogesh Kumar");
+
+        System.out.println("My Address is: XXX");
+    }
+}
