b93a820ba5dbd4e58ffdbb17e60b88aafb26be90
yogk2004 <112416066+yogk2004@users.noreply.github.com>
2022-10-20 23:08:45 +0530

    Problems Masai

--- ProblemSession3/src/Problem4.java
+++ ProblemSession3/src/Problem4.java
@@ -3,6 +3,9 @@ public class Problem4 {
        int LeftYears=Year%400;
        int LeftDays=(Year/400)%7;
 
-       
+       int LeftDays2=(LeftDays/400)%7;
+       int LeftDays3=(LeftDays*7)/5;
+       int LeftDays4=(LeftDays3/5);
+
     }
 }
---
+++ Problems Session Masai/src/Problem1.java
@@ -0,0 +1,11 @@
+public class Problem1 {
+    public static void printMasaiSchool(int num){
+        for(int a = 1; a == num; a++){
+            System.out.println("Masai School");
+        }
+    }
+
+    public static void main(String[] args) {
+        printMasaiSchool(5);
+    }
+}
